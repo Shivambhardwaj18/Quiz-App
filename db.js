00e2fda62878da0e6661938e1c3529b56092b6c2
Shivambhardwaj18 <shivambhardawaj0129@gmai.com>
2021-05-08 16:11:28 +0530

    First Commit

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL, {
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+module.exports = { connect };
